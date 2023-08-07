904ca39577d9a085f29805571787829aa118d16e
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-08-07 10:07:12 +0200

    Add a way to exit program when control-c is pressed in basic example at first we poll to know if there is an event, then we read this event and we finally use the is_control_c function to check whetther it is this event

--- game_of_life/examples/basic.rs
+++ game_of_life/examples/basic.rs
@@ -1,5 +1,4 @@
-use std::time::Duration;
-use std::thread::sleep;
+use std::{time::Duration, thread::sleep, process};
 use gol_lib::*;
 use crossterm;
 
@@ -20,8 +19,14 @@ fn main(){
 		g.update();
 		refresh();
 		sleep(Duration::from_millis(500));
+		// To end the program if control c is pressed
+		if crossterm::event::poll(Duration::from_secs(0)).unwrap() {
+			if is_ctrl_c(crossterm::event::read().unwrap()).unwrap() {
+				crossterm::terminal::disable_raw_mode().unwrap();
+				leave_alternate().unwrap();
+				process::exit(0);
+			}
+		}
 		clear();
 	}
-	crossterm::terminal::disable_raw_mode().unwrap();
-	leave_alternate().unwrap();
 }
