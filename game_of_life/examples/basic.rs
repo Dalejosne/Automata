8c1fa8c8371a0c926461b057a2ee2d1f38dcdebf
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-08-07 09:31:51 +0200

    update basic with the new functions

--- game_of_life/examples/basic.rs
+++ game_of_life/examples/basic.rs
@@ -3,6 +3,8 @@ use std::thread::sleep;
 use gol_lib::*;
 
 fn main(){
+	enter_alternate().unwrap();
+	hide_cursor();
 	let mut g = GameOfLife::new(45, 25);
 	g.set_element(5, 5);
 	g.set_element(6, 5);
@@ -14,7 +16,8 @@ fn main(){
 		g.show();
 		g.update();
 		refresh();
-		clear();
 		sleep(Duration::from_millis(500));
+		clear();
 	}
+	leave_alternate().unwrap();
 }
